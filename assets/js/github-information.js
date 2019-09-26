6bf5fab41f90aaad1cdbc1548bebba06bf85c7f1
Ubuntu <ubuntu@ip-172-31-84-27.ec2.internal>
2019-09-26 16:20:32 +0000

    Final commit

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -37,7 +37,8 @@ function repoInformationHTML(repos) {
 }
 
 function fetchGitHubInformation(event) {
-
+    $("#gh-user-data").html("");
+    $("#gh-repo-data").html("");
     var username = $("#gh-username").val();
     if (!username) {
         $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -63,11 +64,15 @@ function fetchGitHubInformation(event) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(
                     `<h2>No info found for user ${username}</h2>`);
-            }
-            else {
+            } else if(errorResponse === 403){
+                var resetTime= new Date(errorResponse.getResponceHeader('X-RateLimit-Reset')*1000);
+                $("#gh-user-data").html(`<h4>Too many requests, please wait until ${resetTime.toLocaleTimeString()}</h4>`);
+            }  else {
                 console.log(errorResponse);
                 $("#gh-user-data").html(
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
 }
+
+$(document).ready(fetchGitHubInformation);
